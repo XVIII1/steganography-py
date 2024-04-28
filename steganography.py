af24370d576d6a7b0c10de17d1ea29f07cbd43fc
SysMurff <ynhoffman@gmail.com>
2024-04-28 22:24:49 +0300

    add decode / encode fnctionality

--- steganography.py
+++ steganography.py
@@ -2,21 +2,24 @@ import numpy
 from PIL import Image
 
 
-def Encode(src, msg, dest):
+RGB = 'RGB'
+RGBA = 'RGBA'
+
+
+def Encode(src, msg, dest, lsb_tag='$t3g0', switch='08b'):
     raw = Image.open(src, 'r')
     h, w = raw.size
     arr = numpy.array(list(raw.getdata()))
-    if raw.mode == 'RGB':
+    if raw.mode == RGB:
         n = 3
-    elif raw.mode == 'RGBA':
+    elif raw.mode == RGBA:
         n = 4
     total_pixels = arr.size//n
-    msg += '$t3g0'
-    b_msg = ''.join([format(ord(i), '08b') for i in msg])
+    msg += lsb_tag
+    b_msg = ''.join([format(ord(i), switch) for i in msg])
     req_pixels = len(b_msg)
     if req_pixels > total_pixels:
         print("ERROR: Need larger file size")
-
     else:
         index = 0
         for p in range(total_pixels):
@@ -32,31 +35,26 @@ def Encode(src, msg, dest):
     print("Image Encoded Successfully")
 
 
-def Decode(src):
-
+def Decode(src, lsb_tag="$t3g0"):
     img = Image.open(src, 'r')
     array = numpy.array(list(img.getdata()))
-
-    if img.mode == 'RGB':
+    if img.mode == RGB:
         n = 3
-    elif img.mode == 'RGBA':
+    elif img.mode == RGBA:
         n = 4
     total_pixels = array.size//n
-
     hidden_bits = ""
     for p in range(total_pixels):
         for q in range(0, 3):
             hidden_bits += (bin(array[p][q])[2:][-1])
-
     hidden_bits = [hidden_bits[i:i+8] for i in range(0, len(hidden_bits), 8)]
-
     message = ""
     for i in range(len(hidden_bits)):
-        if message[-5:] == "$t3g0":
+        if message[-len(lsb_tag):] == lsb_tag:
             break
         else:
             message += chr(int(hidden_bits[i], 2))
-    if "$t3g0" in message:
+    if lsb_tag in message:
         print("Hidden Message:", message[:-5])
     else:
         print("No Hidden Message Found")
